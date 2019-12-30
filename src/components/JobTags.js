2dabf18ccde966d7db1a25aad2be1727434e6643
Cato Perez <perezcatoc@gmail.com>
2019-12-30 02:25:27 +0000

    almost done with the redux

---
+++ src/components/JobTags.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const JobDetails = () => {
+    return (<div className="w-full border border-blue-700 shadow-outline rounded">
+        this is the job details
+    </div>)
+}
